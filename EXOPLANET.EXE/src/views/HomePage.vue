5ea24f12133fa456a3134e4a734c99b1e817544f
labbaci <labbaci@student.chalmers.se>
2024-10-05 20:29:44 +0200

    Fix more bugs

--- EXOPLANET.EXE/src/views/HomePage.vue
+++ EXOPLANET.EXE/src/views/HomePage.vue
@@ -17,13 +17,13 @@
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-    setup() {
-
-    },
-})
+  setup() {
+    // Any logic you need can be placed here.
+  }
+});
 </script>
 
 <style scoped>
