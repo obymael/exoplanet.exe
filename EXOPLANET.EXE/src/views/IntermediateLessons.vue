bc2e52a40f0dcf8da2e3d2daabc5d5c345570b9f
labbaci <labbaci@student.chalmers.se>
2024-10-05 23:26:24 +0200

    #15 Add animate planets

--- EXOPLANET.EXE/src/views/IntermediateLessons.vue
+++ EXOPLANET.EXE/src/views/IntermediateLessons.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="intermediate-container">
       <section id="index-section">
-        <h1>Intermediate Level</h1>
-        <div class="links-wrapper">
-            <h5>Lessons:</h5>
-            <div class="links-to-lessons">
-                <a href="#lesson-1">1. Types of exoplanets</a>
-                <a href="#lesson-2">2. Comparing exoplanets to our Earth</a>
-                <a href="#lesson-3">3. The Habitable Zone</a>
-                <a href="#lesson-4">4. Significance of exoplanets</a>
-                <a href="#lesson-5">5. Planetary form and evolution</a>
+        <div class="border-index border-index-left">
+            <img src="@/assets/planet-image-1.jpg" alt="planet" class="planet-1-image">
+        </div>
+        <div class="center-index">
+            <h1>Intermediate Level</h1>
+            <div class="links-wrapper">
+                <h5>Lessons:</h5>
+                <div class="links-to-lessons">
+                    <a href="#lesson-1">1. Types of exoplanets</a>
+                    <a href="#lesson-2">2. Comparing exoplanets to our Earth</a>
+                    <a href="#lesson-3">3. The Habitable Zone</a>
+                    <a href="#lesson-4">4. Significance of exoplanets</a>
+                    <a href="#lesson-5">5. Planetary form and evolution</a>
+                </div>
             </div>
+        </div>
+        <div class="border-index border-index-right">
+            <img src="@/assets/planet-image-2.jpg" alt="planet" class="planet-2-image">
         </div>
       </section>
       <section id="lesson-1">
@@ -53,12 +61,42 @@ h5 {
 }
 
 #index-section{
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 10fr 1fr;
     justify-content: center;
     align-items: center;
-    border: 2px solid yellow;
     gap: 2rem;
     padding-top: 7rem !important;
+}
+
+.planet-2-image{
+    margin-left: 1rem;
+    max-width: 8rem;
+}
+
+.planet-1-image{
+    max-height: 10rem;
+}
+
+.planet-1-image,
+.planet-2-image{
+    border-radius: 50%;
+    background-color: #6b5c987c ;
+    box-shadow: 0 0 7rem #6b5c987c;
+    animation: floatImage 4s ease-in-out infinite;
+}
+
+
+@keyframes floatImage{
+    0%{
+        transform: translateY(0);
+    }
+    50%{
+        transform: translateY(-5rem);
+    }
+    100%{
+        transform: translateY(0);
+    }
 }
 
 .links-wrapper{
