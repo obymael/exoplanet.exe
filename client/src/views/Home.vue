1d6db07cef3d6645f329fa76546856322efcb5e2
labbaci <labbaci@student.chalmers.se>
2024-10-05 12:00:02 +0200

    #2 Begin basic structure of Home.vue

---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="body-container">
+        <header class="header"></header>
+        <div class="welcome-container"></div>
+        <div class="cards-container"></div>
+        <footer class="footer"></footer>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
