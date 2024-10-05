d1a21e59739b22d9b8658b0731af40efea9e08e3
labbaci <labbaci@student.chalmers.se>
2024-10-05 17:15:55 +0200

    #2 Create text and containers, fix colours

--- EXOPLANET.EXE/src/views/HomePage.vue
+++ EXOPLANET.EXE/src/views/HomePage.vue
@@ -2,9 +2,16 @@
     <div class="body-container">
         <header class="header"></header>
         <div class="welcome-container">
-            <h1 class="title">Hello</h1>
+            <h1 class="title-text">Learn with Eclipse!</h1>
+            <p class="intro-text">We have different learning levels which you can choose from below.</p>
+            <p class="intro-text">Each level will provide you with fun engaging games and 
+            challenges related to the lessons.</p>            
         </div>
-        <div class="cards-container"></div>
+        <div class="cards-container">
+            <div class="single-card-wrapper"></div>
+            <div class="single-card-wrapper"></div>
+            <div class="single-card-wrapper"></div>
+        </div>
         <footer class="footer"></footer>
     </div>
 </template>
@@ -20,5 +27,30 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.welcome-container{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding-top: 3rem;
+}
+h1 {
+    color: #B3C9D6;
+    font-size: 3rem;
+}
+.intro-text{
+    color: #DDF3FF;
+}
+
+.cards-container{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 20px;
+    padding: 7rem 9% 2rem;
+    width: 100%;
+}
+
+.single-card-wrapper{
+    border: 1px solid red;
+}
 
 </style>
