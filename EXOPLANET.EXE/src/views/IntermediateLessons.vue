9ebfdd296b96b896e118d348af6044b4898ade0c
labbaci <labbaci@student.chalmers.se>
2024-10-05 23:01:24 +0200

    #15 Structure the index section

--- EXOPLANET.EXE/src/views/IntermediateLessons.vue
+++ EXOPLANET.EXE/src/views/IntermediateLessons.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="intermediate-container">
-      <section class="index">
+      <section id="index-section">
         <h1>Intermediate Level</h1>
-        <h5>Lessons:</h5>
-        <div class="links-to-lessons">
-            <a href="#lesson-1">1. Types of exoplanets</a>
-            <a href="#lesson-2">2. Comparing exoplanets to our Earth</a>
-            <a href="#lesson-3">3. The Habitable Zone</a>
-            <a href="#lesson-4">4. Significance of exoplanets</a>
-            <a href="#lesson-5">5. Planetary form and evolution</a>
+        <div class="links-wrapper">
+            <h5>Lessons:</h5>
+            <div class="links-to-lessons">
+                <a href="#lesson-1">1. Types of exoplanets</a>
+                <a href="#lesson-2">2. Comparing exoplanets to our Earth</a>
+                <a href="#lesson-3">3. The Habitable Zone</a>
+                <a href="#lesson-4">4. Significance of exoplanets</a>
+                <a href="#lesson-5">5. Planetary form and evolution</a>
+            </div>
         </div>
       </section>
       <section id="lesson-1">
@@ -40,7 +42,46 @@ align-items: flex-start; /* Align items to the start */
 }
 
 h1 {
-font-size: 4rem;
+    font-size: 4rem;
+    color: var(--color-heading);
+    text-shadow: 3px 3px 5px rgba(0, 0, 0, 0.5);
+}
+
+h5 {
+    font-size: 2rem;
+    color: rgb(234, 217, 248);
+}
+
+#index-section{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid yellow;
+    gap: 2rem;
+    padding-top: 7rem !important;
+}
+
+.links-wrapper{
+    background: linear-gradient(rgba(140,222,227,0.4), rgba(219,195,242,0.4));
+    padding: 1.5rem 3rem;
+    border: 1px solid pink;
+}
+.links-to-lessons{
+    display: flex;
+    flex-direction: column;
+}
+
+.links-to-lessons a{
+    font-size: 1.5rem;
+    color: var(--color-light-text);
+    text-decoration: none;
+    transition: all 1s;
+}
+
+.links-to-lessons a:hover{
+    text-decoration: underline wavy aliceblue;
+    color: rgb(234, 217, 248);
+    transform: scale(1.03);
 }
 
 h2 {
