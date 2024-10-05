9f5e80095061b50e078fb837ec261cd9e04dd50d
Julia McCall <mccall@student.chalmers.se>
2024-10-05 17:01:53 +0200

    #8 Add header and footer components to all views

---
+++ EXOPLANET.EXE/src/components/FooterComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer class ="footer">
+        <div class="logo">
+            <img src="../assets/logo.png" alt="Eclipse Logo">
+            <p class="copyright">© 2024 Eclipse Learning Platform. All rights reserved.</p>
+        </div>
+    </footer>
+  </template>
+
+<script>
+export default {
+  name: 'FooterComponent'
+}
+</script>
+
+  <style scoped>
+  .footer {
+    background-color: #13112E;
+    text-align: left;
+  }
+  .logo img {
+    height: auto;
+    max-height: 30px;
+  }
+  .copyright {
+    margin-left: 10px;
+    margin-bottom: 5px;
+    font-size: 14px;
+    color: #DBC3F2;
+  }
+  </style>
---
+++ EXOPLANET.EXE/src/components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <header class="header p-3">
+        <div class="logo">
+          <!-- Link to home page once implemented -->
+           <!-- <router-link to="/">Home</router-link> -->
+            <img src="../assets/logo.png" alt="Eclipse Logo" />
+        </div>
+      </header>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeaderComponent'
+  }
+  </script>
+  
+    <style scoped>
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #13112E;
+  }
+   .logo img {
+    max-width: 100%;
+    height: auto;
+    max-height: 70px;
+  }
+  </style>
+  
