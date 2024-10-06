73c073d890642041a37ecabb8336268ed7f2a51e
Saba Legesse <guslegessa@student.gu.se>
2024-10-06 12:13:27 +0200

    #18: Adding the learnmore page

---
+++ EXOPLANET.EXE/src/views/LearnMore1.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="intermediate-container">
+        <section id="lesson-1-full" class="learn-more-section">
+            <div class="learn-more-container">
+                <div class="learn-more-images">
+                    <img src="@/assets/planet-image-1.jpg" alt="Planet Image 1" class="learn-more-image">
+                    <img src="@/assets/planet-image-2.jpg" alt="Planet Image 2" class="learn-more-image">
+                </div>
+                <div class="learn-more-text">
+                    <h2>More About Types of Exoplanets</h2>
+                    <p>Here is more detailed information about the different types of exoplanets, including gas giants, Neptunian planets, and more!</p>
+                </div>
+            </div>
+        </section>
+
+    </div>
+</template>
